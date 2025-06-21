84063e935f06578c39fc8948c5a4efec934de3e0
u_0233c <vesmirnov@alfabank.ru>
2025-06-21 12:00:06 +0300

    add route

--- internal/handlers/EmployeeHandler.go
+++ internal/handlers/EmployeeHandler.go
@@ -9,20 +9,24 @@ import (
 )
 
 type EmployeeHandler struct {
-	service *services.EmployeeService
+	service services.EmployeeInterface
 }
 
-func (h *EmployeeHandler) GetEmployeeInfo(id string) (models.Employee, error){
+func NewEmployeeHandler(s services.EmployeeInterface) *EmployeeHandler {
+	return &EmployeeHandler{service: s}
+}
+
+func (h *EmployeeHandler) GetEmployeeInfo(id string) (models.Employee, error) {
 	return h.service.GetEmployeeInfo(id)
 }
 
 func (h *EmployeeHandler) GetEmployee(c *gin.Context) {
 	id := c.Param("id")
 	empl, err := h.service.GetEmployeeInfo(id)
-	if err != nil{
-		 c.JSON(http.StatusNotFound, gin.H{"error":"not found"})
-		 return
-	} 
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "not found"})
+		return
+	}
 	c.JSON(http.StatusOK, empl)
 
 }
